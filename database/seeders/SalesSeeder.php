4719159dbbd183150131eb501230dffa6c24f027
AndresFelipe11 <andres.1701714898@gmail.com>
2023-05-08 15:23:28 -0500

    Update

--- database/seeders/SalesSeeder.php
+++ database/seeders/SalesSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Sale;
+use DateTime;
+use Illuminate\Support\Facades\DB;
 
 class SalesSeeder extends Seeder
 {
@@ -14,6 +16,33 @@ class SalesSeeder extends Seeder
      */
     public function run()
     {
-        DB::create
+        DB::table('sales')->insert([
+        [
+            'nombre_empleado'=> 'Juan Fernando',
+            'nombre_cliente' => 'Pepito Perez',
+            'nombre_producto' => 'Camisa de colores',
+            'precio' => '15000',
+            'fecha_compra' => new DateTime(),
+
+        ],
+        
+        [
+            'nombre_empleado'=> 'Carlitos',
+            'nombre_cliente' => 'Pepito Perez',
+            'nombre_producto' => 'Camisa de colores',
+            'precio' => '20000',
+            'fecha_compra' => new DateTime(),
+        ],
+
+        [
+            'nombre_empleado'=> 'Julian',
+            'nombre_cliente' => 'Pepito Perez',
+            'nombre_producto' => 'Camisa de colores',
+            'precio' => '12000',
+            'fecha_compra' => new DateTime(),
+        
+        ]
+        ]);
+    
     }
 }
